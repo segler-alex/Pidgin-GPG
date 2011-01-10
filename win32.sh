6caff78166a488a333be5b647bbb15a8fe65a58e
segler_alex <segler_alex@web.de>
2011-01-10 19:14:27 +0100

    made win32.sh more general

--- win32.sh
+++ win32.sh
@@ -1,3 +1,4 @@
 ./configure --host i586-mingw32msvc
 make clean
-make LDFLAGS="-no-undefined -L/mnt/data/segler/pidgin-gpg/Pidgin-GPG/win32libs/"
+DIR=`pwd`
+make LDFLAGS="-no-undefined -L$DIR/win32libs"
